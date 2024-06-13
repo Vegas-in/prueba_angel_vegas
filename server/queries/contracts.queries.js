f34eb5c7373ec7b0f807e16a175b74e51e5c0053
Vegas-in <angeldevegas@gmail.com>
2024-06-13 21:18:26 +0200

    cambios en controlador, modelo y consulta para poder filtrar con varios parámetros a la vez(cambio de estrategia)

--- server/queries/contracts.queries.js
+++ server/queries/contracts.queries.js
@@ -1,13 +1,8 @@
 const queries = {
-    getContracts: `
+    getAllContracts: `
         SELECT *
         FROM contratos
-        WHERE
-            (? IS NULL OR Id = ?)
-            OR (? IS NULL OR origen = ?)
-            OR (? IS NULL OR estado = ?)
-            OR (? IS NULL OR cupon = ?)
-        LIMIT 50
+        WHERE 1=1
     `,
 };
 
